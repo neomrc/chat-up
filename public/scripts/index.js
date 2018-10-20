601df9f6b38a4ad6604fee15e8188c465144482a
Dominic Pioquinto <dominic@incube8.sg>
2018-10-20 12:35:10 +0800

    removed socket io and added travis integration

--- public/scripts/index.js
+++
@@ -1,8 +0,0 @@
-(function(isSocketEnabled) {
-  if (!isSocketEnabled) throw new Error('Socket is not enabled');
-  var socket = io(window.location.href);
-
-  socket.on('availableRooms', function(rooms) {
-    console.log(rooms);
-  });
-})(typeof io !== 'undefined');
